48bb7096c4fe65f1ab806257dc99339cd492d951
bulnabang99 <bulnabang99@gmail.com>
2023-08-10 22:34:40 +0900

    FEAT: 게시판 서비스 클래스 생성

---
+++ src/main/java/com/wanted/service/BoardService.java
@@ -0,0 +1,13 @@
+package com.wanted.service;
+
+
+import com.wanted.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+}
