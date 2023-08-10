6896a765dd78c20066ba1a674bc46fe57a5fe25c
bulnabang99 <bulnabang99@gmail.com>
2023-08-10 22:35:08 +0900

    FEAT: 게시판 리포지토리 인터페이스 생성

---
+++ src/main/java/com/wanted/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.wanted.repository;
+
+import com.wanted.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
