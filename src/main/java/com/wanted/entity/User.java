1418941aa592ad4b061468dc593f54c9bc83b7fd
bulnabang99 <bulnabang99@gmail.com>
2023-08-12 01:19:34 +0900

    FEAT: 유저 엔티티 클래스 생성

---
+++ src/main/java/com/wanted/entity/User.java
@@ -0,0 +1,30 @@
+package com.wanted.entity;
+
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Builder
+@Table(name = "user")
+@DynamicInsert
+@DynamicUpdate
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
