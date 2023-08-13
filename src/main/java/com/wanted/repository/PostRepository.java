534c53e6576badd82ef6601b3da8c102440abe7b
wonseok970 <bulnabang99@gmail.com>
2023-08-13 17:34:33 +0900

    FIX: JPARepository 이름 수정 Board -> Post

--- src/main/java/com/wanted/repository/PostRepository.java
+++ src/main/java/com/wanted/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package com.wanted.repository;
 
-import com.wanted.entity.Board;
+import com.wanted.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
