df58d694b2d826c592da63b1ce0943f45f916b9d
bulnabang99 <bulnabang99@gmail.com>
2023-08-15 12:05:17 +0900

    FEAT: Swagger 구성환경 설정

---
+++ src/main/java/com/wanted/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.wanted.config;
+
+import org.springframework.stereotype.Component;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component("/api")
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        final String securitySchemeName = "bearerAuth";
+
+        Info info = new Info().title("wanted").version(appVersion)
+                .description("wanted-pre-onboarding-backend.");
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(info);
+    }
+}
